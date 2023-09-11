f859edd56dc28778a59caa75d59a8cbf2e755dd1
John Afolayan <jnafolayan@gmail.com>
2023-09-11 23:34:56 +0100

    compress jpegs

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,7 +1,14 @@
 rm -r images/
 mkdir images
 
-for file in hires-images/*.png; 
+compress pngs
+for file in hires-images/*.png
 do
-    pngcrush -reduce -brute "$file" "${file//hires-/}"; 
+    pngcrush -reduce -brute "$file" "${file//hires-/}"
+done
+
+# compress jpgs
+for file in hires-images/*.jpg
+do 
+    sips -s format jpeg -s formatOptions low $file --out ${file//hires-/}
 done
